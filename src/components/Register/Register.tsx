c22e0006d3a6205fe4e7c18e2a3837a067e4089e
Gumay <humaivier88@gmail.com>
2021-09-19 20:20:06 +0100

    Add api and testing api

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -6,53 +6,74 @@ import { Input } from "../../UI-kit/Input/Input";
 import styles from "./Register.module.css";
 
 export const Register = () => {
-  return (
-    <div className={styles.wrapper}>
-      <Card className={styles.register}>
-        <h1>It-incubator</h1>
+    return (
+        <div className={styles.wrapper}>
+            <Card className={styles.register}>
+                <h1>It-incubator</h1>
 
-        <div className={styles.formWrapper}>
-          <form action="/">
-            <h2>Sign Up</h2>
+                <div className={styles.formWrapper}>
+                    <form action="/">
+                        <h2>Sign Up</h2>
 
-            <div className={styles.formGroup}>
-              <label>
-                <span>Email</span>
-                <br />
-                <Input
-                  type="text"
-                  placeholder={"j&johnson.gmail.com"}
-                  required
-                />
-              </label>
-            </div>
+                        <div className={styles.formGroup}>
+                            <label>
+                                <span>Email</span>
+                                <br />
+                                <Input
+                                    type="text"
+                                    placeholder={"j&johnson.gmail.com"}
+                                    required
+                                />
+                            </label>
+                        </div>
 
-            <div className={`${styles.formGroup} ${styles.shapeIcon}`}>
-              <label>
-                <span>Password</span>
-                <br />
-                <Input type="password" placeholder={"*******"} required />
-              </label>
-            </div>
-            <div className={`${styles.formGroup} ${styles.shapeIcon}`}>
-              <label>
-                <span>Confirm Password</span>
-                <br />
-                <Input type="password" placeholder={"*******"} required />
-              </label>
-            </div>
+                        <div
+                            className={`${styles.formGroup} ${styles.shapeIcon}`}
+                        >
+                            <label>
+                                <span>Password</span>
+                                <br />
+                                <Input
+                                    type="password"
+                                    placeholder={"*******"}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div
+                            className={`${styles.formGroup} ${styles.shapeIcon}`}
+                        >
+                            <label>
+                                <span>Confirm Password</span>
+                                <br />
+                                <Input
+                                    type="password"
+                                    placeholder={"*******"}
+                                    required
+                                />
+                            </label>
+                        </div>
 
-            <div className={`${styles.formGroup} ${styles.formGroupButton}`}>
-              <Button className={styles.cancelBtn} purple type={"submit"}>
-                Cancel
-              </Button>
-              <Button className={styles.registerBtn} type={"submit"}>
-                Register
-              </Button>
-            </div>
-          </form>
+                        <div
+                            className={`${styles.formGroup} ${styles.formGroupButton}`}
+                        >
+                            <Button
+                                className={styles.cancelBtn}
+                                purple
+                                type={"submit"}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                className={styles.registerBtn}
+                                type={"submit"}
+                            >
+                                Register
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </Card>
         </div>
-      </Card>
-    </div>
-  );
+    );
 };
--- src/components/TestPage/TestPage.tsx
+++ src/components/TestPage/TestPage.tsx
@@ -1,28 +1,90 @@
-import React, {ChangeEvent, useState} from "react";
-import {Button} from "../../UI-kit/Button/Button";
-import {Checkbox} from "../../UI-kit/Checkbox/Checkbox";
-import {EditableSpan} from "../../UI-kit/EditableSpan/EditableSpan";
-import {Input} from "../../UI-kit/Input/Input";
-import {Radio} from "../../UI-kit/Radio/Radio";
-import {Select} from "../../UI-kit/Select/Select";
-import {Range} from "../../UI-kit/Range/Range";
-import {Header} from "../Header/Header";
+import React, { ChangeEvent, useState } from "react";
+import { Button } from "../../UI-kit/Button/Button";
+import { Checkbox } from "../../UI-kit/Checkbox/Checkbox";
+import { EditableSpan } from "../../UI-kit/EditableSpan/EditableSpan";
+import { Input } from "../../UI-kit/Input/Input";
+import { Radio } from "../../UI-kit/Radio/Radio";
+import { Select } from "../../UI-kit/Select/Select";
+import { Range } from "../../UI-kit/Range/Range";
+import { Header } from "../Header/Header";
+import { authAPI } from "../../api/api";
+import styles from "./TestPage.module.css";
 
 export const TestPage = () => {
     const arr: string[] = ["apple", "grape", "cherry"];
     const [value, onChangeOption] = useState(arr[1]);
     const [rangeValue, setRangeValue] = useState(50);
     const [checked, setChecked] = useState<boolean>(false);
+    const [response, setResponse] = useState<any>();
     const testOnChange = (e: ChangeEvent<HTMLInputElement>) => {
         setChecked(e.currentTarget.checked);
     };
     const changeHandler = (value: number) => {
         setRangeValue(value);
     };
-
+    const login = () => {
+        authAPI
+            .login("nya-admn@nya.nya", "1qazxcvBG", true)
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const me = () => {
+        authAPI
+            .me()
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const register = () => {
+        authAPI
+            .register("blabla3@nya.nya", "1qazxcvBG")
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const changeInfo = () => {
+        authAPI
+            .changeInfo("New Name", "https://i.mydramalist.com/67r8d_5_c.jpg")
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const logout = () => {
+        authAPI
+            .logout()
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const forgotPassword = () => {
+        authAPI
+            .forgotPassword("blabla@nya.nya", "gumay88@mail.ru", "My message")
+            .then((res) => {
+                setResponse(JSON.stringify(res.data));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
     return (
         <>
-            <Header/>
+            <Header />
             <div
                 style={{
                     display: "flex",
@@ -34,15 +96,31 @@ export const TestPage = () => {
                 }}
             >
                 <Button>button</Button>
-                <Checkbox checked={checked} onChange={testOnChange}/>
-                <EditableSpan/>
-                <Radio value={value} options={arr} onChangeOption={onChangeOption}/>
-                <Input/>
-                <span style={{fontSize: "1.7rem"}}>{rangeValue}</span>
-                <Range onChangeRange={changeHandler} value={rangeValue}/>
-                <Select value={value} options={arr} onChangeOption={onChangeOption}/>
+                <Checkbox checked={checked} onChange={testOnChange} />
+                <EditableSpan />
+                <Radio
+                    value={value}
+                    options={arr}
+                    onChangeOption={onChangeOption}
+                />
+                <Input />
+                <span style={{ fontSize: "1.7rem" }}>{rangeValue}</span>
+                <Range onChangeRange={changeHandler} value={rangeValue} />
+                <Select
+                    value={value}
+                    options={arr}
+                    onChangeOption={onChangeOption}
+                />
+                <div className={styles.requestsWrapper}>
+                    <Button onClick={login}>Login Request</Button>
+                    <Button onClick={register}>Register</Button>
+                    <Button onClick={me}>Show User</Button>
+                    <Button onClick={changeInfo}>Change Info</Button>
+                    <Button onClick={logout}>Logout</Button>
+                    <Button onClick={forgotPassword}>Forgot Password</Button>
+                </div>
+                <div style={{ width: "800px" }}>{response}</div>
             </div>
         </>
-
     );
 };
