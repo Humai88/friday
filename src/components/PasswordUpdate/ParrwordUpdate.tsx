894141bee93a5ecbfb4b799068fd67a947b6e71b
Gumay <humaivier88@gmail.com>
2021-09-24 13:12:59 +0100

    Add password update form

--- src/components/PasswordUpdate/ParrwordUpdate.tsx
+++ src/components/PasswordUpdate/ParrwordUpdate.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Button } from "../../UI-kit/Button/Button";
+import { Card } from "../../UI-kit/Card/Card";
+import { Input } from "../../UI-kit/Input/Input";
+import styles from "./ParrwordUpdate.module.css";
 
 export const PasswordUpdate = () => {
-  return <h1>Password Update</h1>;
+    return (
+        <div className={styles.wrapper}>
+            <Card className={styles.emailChecking}>
+                <h1>It-incubator</h1>
+                <h2>Create new password</h2>
+                <div className={`${styles.formGroup} ${styles.shapeIcon}`}>
+                    <label>
+                        <span>Password</span>
+                        <br />
+                        <Input type="password" />
+                    </label>
+                    <p>
+                        Create new password and we will send you futher
+                        instructions to email
+                    </p>
+                </div>
+                <Button className={styles.btn}>Create new password</Button>
+            </Card>
+        </div>
+    );
 };
